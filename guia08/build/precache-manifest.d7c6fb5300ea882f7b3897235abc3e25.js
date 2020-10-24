b5ccb9f20e48964802fe7384530ebc0a3d535e68
Oscar Carranza <carranzav_oscar@hotmail.com>
2020-10-24 15:50:35 -0600

    Crud con PHP

---
+++ guia08/build/precache-manifest.d7c6fb5300ea882f7b3897235abc3e25.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80c0929d2d551d47dfe3422e47ba92b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a922a2f4830adb6bb377",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "3a7802f6d5974a1e06a4",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a922a2f4830adb6bb377",
+    "url": "/static/js/2.3a47876d.chunk.js"
+  },
+  {
+    "revision": "78fd0f62364854e39c53711d027f73a3",
+    "url": "/static/js/2.3a47876d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a7802f6d5974a1e06a4",
+    "url": "/static/js/main.206f7762.chunk.js"
+  },
+  {
+    "revision": "4027bb46236991a061cd",
+    "url": "/static/js/runtime-main.83e6c96f.js"
+  }
+]);
